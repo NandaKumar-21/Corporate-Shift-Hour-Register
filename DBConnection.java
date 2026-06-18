1af7a3ec1f09875eb671f24392e2b4f8c3d52277
NANDA KUMAR T <nandakumarcs21@gmail.com>
2026-06-18 12:34:02 +0530

    Database Connection Established

--- DBConnection.java
+++ DBConnection.java
@@ -1,27 +1,25 @@
 import java.sql.Connection;
-
+import java.sql.DriverManager;
 public class DBConnection {
-
-    private static final String URL = "";
-    private static final String USERNAME = "";
-    private static final String PASSWORD = "";
-
+    private static final String url ="jdbc:mysql://localhost:3312/corporate";
+    private static final String username = "root";
+    private static final String password = "";
     public static Connection getConnection() {
-
         Connection con = null;
-
         try {
-
-            // Load Driver
-
-            // Create Connection
-
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(
+                    url,
+                    username,
+                    password
+            );
+            System.out.println("Database Connected Successfully");
         }
         catch(Exception e) {
 
+            System.out.println("Connection failed");
             e.printStackTrace();
         }
-
         return con;
     }
 }
--- Main.java
+++ Main.java
@@ -1,58 +1,9 @@
-import java.util.Scanner;
+import java.sql.Connection;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        EmployeeDAO dao = new EmployeeDAO();
 
-        int choice;
-
-        do {
-            System.out.println("\n===== Corporate Shift Hour Register =====");
-            System.out.println("1. Add Employee Shift");
-            System.out.println("2. Upload CSV");
-            System.out.println("3. View Records");
-            System.out.println("4. Search Employee");
-            System.out.println("5. Generate Report");
-            System.out.println("6. Exit");
-
-            System.out.print("Enter Choice: ");
-            choice = sc.nextInt();
-
-            switch(choice) {
-
-                case 1:
-                    // Add Employee Shift
-                    break;
-
-                case 2:
-                    // Upload CSV
-                    break;
-
-                case 3:
-                    dao.viewAllRecords();
-                    break;
-
-                case 4:
-                    // Search Employee
-                    break;
-
-                case 5:
-                    // Generate Report
-                    break;
-
-                case 6:
-                    System.out.println("Exiting...");
-                    break;
-
-                default:
-                    System.out.println("Invalid Choice");
-            }
-
-        } while(choice != 6);
-
-        sc.close();
     }
 }
