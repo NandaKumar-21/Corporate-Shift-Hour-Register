7c22aafb3e7e97423c07d09bcd363cc3bfdea7f7
NANDA KUMAR T <nandakumarcs21@gmail.com>
2026-06-18 13:01:25 +0530

    simple code logic for calculating Over time

--- EmployeeShift.java
+++ EmployeeShift.java
@@ -1,5 +1,4 @@
 public class EmployeeShift {
-    private int slNo;
     private String empId;
     private String empName;
     private String domain;
@@ -10,18 +9,66 @@ public class EmployeeShift {
     private double overtimePay;
     public EmployeeShift() {
     }
-    public EmployeeShift(
-            String empId,
-            String empName,
-            String domain,
-            String inTime,
-            String outTime) {
-
+    public EmployeeShift(String empId, String empName, String domain,
+                         String inTime, String outTime,
+                         double workingHours,
+                         double overtimeHours,
+                         double overtimePay) {
+        this.empId = empId;
+        this.empName = empName;
+        this.domain = domain;
+        this.inTime = inTime;
+        this.outTime = outTime;
+        this.workingHours = workingHours;
+        this.overtimeHours = overtimeHours;
+        this.overtimePay = overtimePay;
     }
-
-    // Getters
-
-    // Setters
-
-    // toString()
+    public String getEmpId() {
+        return empId;
+    }
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+    public String getEmpName() {
+        return empName;
+    }
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+    public String getDomain() {
+        return domain;
+    }
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+    public String getInTime() {
+        return inTime;
+    }
+    public void setInTime(String inTime) {
+        this.inTime = inTime;
+    }
+    public String getOutTime() {
+        return outTime;
+    }
+    public void setOutTime(String outTime) {
+        this.outTime = outTime;
+    }
+    public double getWorkingHours() {
+        return workingHours;
+    }
+    public void setWorkingHours(double workingHours) {
+        this.workingHours = workingHours;
+    }
+    public double getOvertimeHours() {
+        return overtimeHours;
+    }
+    public void setOvertimeHours(double overtimeHours) {
+        this.overtimeHours = overtimeHours;
+    }
+    public double getOvertimePay() {
+        return overtimePay;
+    }
+    public void setOvertimePay(double overtimePay) {
+        this.overtimePay = overtimePay;
+    }
 }
